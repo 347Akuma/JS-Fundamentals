5b099ec9330334756a9c3b537622cf720247346b
Alexander Dimitrov <adimitrovx@gmail.com>
2024-01-22 00:23:36 +0200

    -

---
+++ 05. Exercise-Data-Types-Variables/01-sum-digits.js
@@ -0,0 +1,11 @@
+function sumDigits(number){
+
+    let sum = 0;
+    while (number != 0){
+        let lastDigit = number % 10;
+        sum += lastDigit;
+        number = parseInt(number / 10);
+    }
+    console.log(sum);
+}
+sumDigits(245678);
